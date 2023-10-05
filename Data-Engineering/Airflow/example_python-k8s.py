db1e037c21617f57da970f4dea63bb11b9fc83eb
Arkadii Kravchuk <64816141+akravacyber@users.noreply.github.com>
2023-10-05 20:16:08 +0000

    test

--- Data-Engineering/Airflow/example_python-k8s.py
+++ Data-Engineering/Airflow/example_python-k8s.py
@@ -31,19 +31,14 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG(
     'kubernetes_sample',
     default_args=default_args,
-    schedule_interval=timedelta(minutes=3),
+    schedule_interval='*/5 * * * *',
+    dagrun_timeout=timedelta(seconds=5),
     tags=['example'],
     access_control={
         'All': {
